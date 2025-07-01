f449d72b1caf2ea6819fbe8ebdc52e6fad9a9969
JefteCosta <jefte@meetrox.ai>
2025-06-30 23:43:14 -0300

    fix(tsconfig): erro ao testar as configuracoes de ts
    #46

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'bun:test';
-import { hello } from '../src/index';
+import { hello } from '../src/index.ts';
 
 test('retorna saudacao', () => {
   expect(hello('Mundo')).toBe('Olá, Mundo!');
